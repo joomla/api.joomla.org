20d984630e5038e757c99149673669d9034252da
Michael Babker <mbabker@flbab.com>
2016-03-19 13:03:28 -0400

    Restructure template JavaScript

--- js/template.js
+++ js/template.js
@@ -1,222 +1,230 @@
-$.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
-$.browser.ipad = /ipad/.test(navigator.userAgent.toLowerCase());
-
-/**
- * Initializes page contents for progressive enhancement.
- */
-function initializeContents() {
-    // hide all more buttons because they are not needed with JS
-    $(".element a.more").hide();
-
-    // make the entire element linkable
-    $(".clickable.class,.clickable.interface,.clickable.trait").click(function () {
-        document.location = $("a.more", this).attr('href');
-    });
-
-    // change the cursor to a pointer to make it more explicit that this it clickable
-    // do a background color change on hover to emphasize the clickability eveb more
-    // we do not use CSS for this because when JS is disabled this behaviour does not
-    // apply and we do not want the hover
-    // @TODO - Add .element.function and .element.constant back into this when they have proper pages
-    $(".element.method,.element.class.clickable,.element.interface.clickable,.element.trait.clickable,.element.property.clickable,.element.function.clickable,.element.constant.clickable")
-        .css("cursor", "pointer")
-        .hover(function () {
-            $(this).css('backgroundColor', '#F8FDF6')
-        }, function () {
-            $(this).css('backgroundColor', 'white')
-        }
-    );
-
-    $("ul.side-nav.nav.nav-list li.nav-header").contents()
-        .filter(function () {
-            return this.nodeType === 3
-        })
-        .wrap('<span class="side-nav-header" />');
-
-    $("ul.side-nav.nav.nav-list li.nav-header span.side-nav-header")
-        .css("cursor", "pointer");
-
-    // do not show tooltips on iPad; it will cause the user having to click twice
-    if (!$.browser.ipad) {
-        $('.btn-group.visibility,.btn-group.view,.btn-group.type-filter,.icon-custom')
-            .tooltip({'placement': 'bottom'});
-        $('.element').tooltip({'placement': 'left'});
-    }
-
-    $('.btn-group.visibility,.btn-group.view,.btn-group.type-filter')
-        .show()
-        .find('button')
-        .find('i').click(function () {
-            $(this).parent().click();
-        });
-
-    // set the events for the visibility buttons and enable by default.
-    $('.visibility button.public').click(function () {
-        $('.element.public,.side-nav li.public').toggle($(this).hasClass('active'));
-    }).click();
-    $('.visibility button.protected').click(function () {
-        $('.element.protected,.side-nav li.protected').toggle($(this).hasClass('active'));
-    }).click();
-    $('.visibility button.private').click(function () {
-        $('.element.private,.side-nav li.private').toggle($(this).hasClass('active'));
-    }).click();
-    $('.visibility button.inherited').click(function () {
-        $('.element.inherited,.side-nav li.inherited').toggle($(this).hasClass('active'));
-    }).click();
-
-    $('.type-filter button.critical').click(function () {
-        $('tr.critical').toggle($(this).hasClass('active'));
-    });
-    $('.type-filter button.error').click(function () {
-        $('tr.error').toggle($(this).hasClass('active'));
-    });
-    $('.type-filter button.notice').click(function () {
-        $('tr.notice').toggle($(this).hasClass('active'));
-    });
-
-    $('ul.side-nav.nav.nav-list li.nav-header span.side-nav-header').click(function () {
-        $(this).siblings('ul').collapse('toggle');
-    });
+if (typeof jQuery === 'undefined') {
+    throw new Error('JoomlaApi JavaScript requires jQuery')
 }
 
-$(document).ready(function () {
-    var navTop;
-    var isFixed = false;
+!function (jQuery, window, document, navigator) {
+    'use strict';
 
-    prettyPrint();
-    initializeContents();
-    processScrollInit();
-    processScroll();
+    jQuery.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase());
+    jQuery.browser.ipad = /ipad/.test(navigator.userAgent.toLowerCase());
 
-    // do not show tooltips on iPad; it will cause the user having to click twice
-    if (!$.browser.ipad) {
-        $(".side-nav a").tooltip({'placement': 'top'});
-    }
+    var JoomlaApi = window.JoomlaApi = {
+        navTop: null,
+        isFixed: false,
 
-    // chrome cannot deal with certain situations; warn the user about reduced features
-    if ($.browser.chrome && (window.location.protocol == 'file:')) {
-        $("body > .container").prepend(
-            '<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a>' +
-            'You are using Google Chrome in a local environment; AJAX interaction has been ' +
-            'disabled because Chrome cannot <a href="http://code.google.com/p/chromium/issues/detail?id=40787">' +
-            'retrieve files using Ajax</a>.</div>'
-        );
-    }
+        /**
+         * Initializes page contents for progressive enhancement.
+         */
+        initializeContents: function () {
+            // hide all more buttons because they are not needed with JS
+            jQuery('.element a.more').hide();
 
-    $('ul.nav-namespaces li a, ul.nav-packages li a').click(function () {
-        // Google Chrome does not do Ajax locally
-        if ($.browser.chrome && (window.location.protocol == 'file:')) {
-            return true;
-        }
+            // make the entire element linkable
+            jQuery('.clickable.class,.clickable.interface,.clickable.trait').click(function () {
+                document.location = jQuery('a.more', this).attr('href');
+            });
 
-        $(this).parents('.side-nav').find('.active').removeClass('active');
-        $(this).parent().addClass('active');
-        $('div.namespace-contents').load(
-            this.href + ' div.namespace-contents', function () {
-                initializeContents();
-                $(window).scrollTop($('div.body').position().top);
+            // change the cursor to a pointer to make it more explicit that this it clickable
+            // do a background color change on hover to emphasize the clickability eveb more
+            // we do not use CSS for this because when JS is disabled this behaviour does not
+            // apply and we do not want the hover
+            // @TODO - Add .element.function and .element.constant back into this when they have proper pages
+            jQuery('.element.method, .element.class.clickable, .element.interface.clickable, .element.trait.clickable, .element.property.clickable, .element.function.clickable, .element.constant.clickable')
+                .css('cursor', 'pointer')
+                .hover(function () {
+                    jQuery(this).css('backgroundColor', '#F8FDF6')
+                }, function () {
+                    jQuery(this).css('backgroundColor', 'white')
+                });
+
+            jQuery('ul.side-nav.nav.nav-list li.nav-header').contents()
+                .filter(function () {
+                    return this.nodeType === 3
+                })
+                .wrap('<span class="side-nav-header" />');
+
+            // do not show tooltips on iPad; it will cause the user having to click twice
+            if (!jQuery.browser.ipad) {
+                jQuery('.btn-group.visibility, .btn-group.type-filter, .icon-custom')
+                    .tooltip({'placement': 'bottom'});
+                jQuery('.element').tooltip({'placement': 'left'});
             }
-        );
-        $('div.package-contents').load(
-            this.href + ' div.package-contents', function () {
-                initializeContents();
-                $(window).scrollTop($('div.body').position().top);
-            }
-        );
 
-        return false;
-    });
+            jQuery('.btn-group.visibility, .btn-group.type-filter').show().find('button').find('i').click(function () {
+                jQuery(this).parent().click();
+            });
 
-    var locationPath = filterPath(location.pathname);
+            // set the events for the visibility buttons and enable by default.
+            jQuery('.visibility').on('click', 'button.public', function () {
+                jQuery('.element.public, .side-nav li.public').toggle(jQuery(this).hasClass('active'));
+            }).on('click', 'button.protected', function () {
+                jQuery('.element.protected, .side-nav li.protected').toggle(jQuery(this).hasClass('active'));
+            }).on('click', 'button.private', function () {
+                jQuery('.element.private, .side-nav li.private').toggle(jQuery(this).hasClass('active'));
+            }).on('click', 'button.inherited', function () {
+                jQuery('.element.inherited, .side-nav li.inherited').toggle(jQuery(this).hasClass('active'));
+            });
 
-    // the ipad already smoothly scrolls and does not detect the scrollable
-    // element if top=0; as such we disable this behaviour for the iPad
-    if (!$.browser.ipad) {
-        $('a[href*=#]').each(function () {
-            var thisPath = filterPath(this.pathname) || locationPath;
-            if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
-                var target = decodeURIComponent(this.hash.replace(/#/, ''));
-                // note: I'm using attribute selector, because id selector can't match elements with '$'
-                var $target = $('[id="' + target + '"]');
+            jQuery('.visibility button.public').click();
+            jQuery('.visibility button.protected').click();
+            jQuery('.visibility button.private').click();
+            jQuery('.visibility button.inherited').click();
 
-                if ($target.length > 0) {
-                    $(this).click(function (event) {
-                        var scrollElem = scrollableElement('html', 'body');
-                        var targetOffset = $target.offset().top;
+            jQuery('.type-filter').on('click', 'button.critical', function () {
+                jQuery('tr.critical').toggle(jQuery(this).hasClass('active'));
+            }).on('click', 'button.error', function () {
+                jQuery('tr.error').toggle(jQuery(this).hasClass('active'));
+            }).on('click', 'button.notice', function () {
+                jQuery('tr.notice').toggle(jQuery(this).hasClass('active'));
+            });
 
-                        event.preventDefault();
-                        $(scrollElem).animate({scrollTop: targetOffset}, 400, function () {
-                            location.hash = target;
-                        });
+            jQuery('ul.side-nav.nav.nav-list li.nav-header span.side-nav-header').css('cursor', 'pointer').click(function () {
+                jQuery(this).siblings('ul').collapse('toggle');
+            });
+        },
+
+        /**
+         * Filter a path
+         *
+         * @param {String} string
+         * @returns {String}
+         */
+        filterPath: function (string) {
+            return string
+                .replace(/^\//, '')
+                .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+                .replace(/\/$/, '');
+        },
+
+        /**
+         * Initialize the scroll listeners
+         */
+        processScrollInit: function () {
+            var subnavWrapper = jQuery('.subnav-wrapper');
+
+            if (subnavWrapper.length) {
+                JoomlaApi.navTop = subnavWrapper.length && subnavWrapper.offset().top - 30;
+
+                // Fix the container top
+                jQuery('.body .container-main').css('top', subnavWrapper.height() + jQuery('nav.navbar').height());
+
+                // Only apply the scrollspy when the toolbar is not collapsed
+                if (document.body.clientWidth > 480) {
+                    var subnav = jQuery('.subnav');
+
+                    subnavWrapper.height(subnav.height());
+                    subnav.affix({
+                        offset: {top: subnav.offset().top - jQuery('nav.navbar').height()}
                     });
                 }
             }
-        });
-    }
+        },
 
-    // Hide API Documentation menu if it's empty
-    $('.subnav .dropdown a[id=api]').next().filter(function (el) {
-        if ($(el).children().length == 0) {
-            return true;
-        }
-    }).parent().hide();
+        /**
+         * Event for when the page is scrolled
+         */
+        processScroll: function () {
+            var subnav = jQuery('.subnav-wrapper');
 
-    function filterPath(string) {
-        return string
-            .replace(/^\//, '')
-            .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
-            .replace(/\/$/, '');
-    }
+            if (subnav.length) {
+                var scrollTop = jQuery(window).scrollTop();
 
-    function processScrollInit() {
-        if ($('.subnav-wrapper').length) {
-            navTop = $('.subnav-wrapper').length && $('.subnav-wrapper').offset().top - 30;
+                if (scrollTop >= JoomlaApi.navTop && !JoomlaApi.isFixed) {
+                    JoomlaApi.isFixed = true;
+                    subnav.addClass('subhead-fixed');
 
-            // Fix the container top
-            $('.body .container-main').css('top', $('.subnav-wrapper').height() + $('nav.navbar').height());
-
-            // Only apply the scrollspy when the toolbar is not collapsed
-            if (document.body.clientWidth > 480) {
-                $('.subnav-wrapper').height($('.subnav').height());
-                $('.subnav').affix({
-                    offset: {top: $('.subnav').offset().top - $('nav.navbar').height()}
-                });
+                    // Fix the container top
+                    jQuery('.body .container-main').css('top', subnav.height() + jQuery('nav.navbar').height());
+                } else if (scrollTop <= JoomlaApi.navTop && JoomlaApi.isFixed) {
+                    JoomlaApi.isFixed = false;
+                    subnav.removeClass('subhead-fixed');
+                }
             }
-        }
-    }
+        },
 
-    function processScroll() {
-        if ($('.subnav-wrapper').length) {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop >= navTop && !isFixed) {
-                isFixed = true;
-                $('.subnav-wrapper').addClass('subhead-fixed');
+        /**
+         * Find the first scrollable element
+         *
+         * @param {*} els
+         * @returns {*}
+         */
+        scrollableElement: function (els) {
+            for (var i = 0, argLength = arguments.length; i < argLength; i++) {
+                var el = arguments[i],
+                    $scrollElement = jQuery(el);
 
-                // Fix the container top
-                $('.body .container-main').css('top', $('.subnav-wrapper').height() + $('nav.navbar').height());
-            } else if (scrollTop <= navTop && isFixed) {
-                isFixed = false;
-                $('.subnav-wrapper').removeClass('subhead-fixed');
-            }
-        }
-    }
+                if ($scrollElement.scrollTop() > 0) {
+                    return el;
+                }
 
-    // use the first element that is "scrollable"
-    function scrollableElement(els) {
-        for (var i = 0, argLength = arguments.length; i < argLength; i++) {
-            var el = arguments[i], $scrollElement = $(el);
-            if ($scrollElement.scrollTop() > 0) {
-                return el;
-            }
-            else {
                 $scrollElement.scrollTop(1);
+
                 var isScrollable = $scrollElement.scrollTop() > 0;
+
                 $scrollElement.scrollTop(0);
+
                 if (isScrollable) {
                     return el;
                 }
             }
+
+            return [];
         }
-        return [];
-    }
-});
+    };
+
+    jQuery(document).ready(function () {
+        prettyPrint();
+        JoomlaApi.initializeContents();
+        JoomlaApi.processScrollInit();
+        JoomlaApi.processScroll();
+
+        // do not show tooltips on iPad; it will cause the user having to click twice
+        if (!jQuery.browser.ipad) {
+            jQuery('.side-nav a').tooltip({'placement': 'top'});
+        }
+
+        // chrome cannot deal with certain situations; warn the user about reduced features
+        if (jQuery.browser.chrome && (window.location.protocol == 'file:')) {
+            jQuery('body > .container').prepend(
+                '<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a>' +
+                'You are using Google Chrome in a local environment; AJAX interaction has been ' +
+                'disabled because Chrome cannot <a href="http://code.google.com/p/chromium/issues/detail?id=40787">' +
+                'retrieve files using Ajax</a>.</div>'
+            );
+        }
+
+        var locationPath = JoomlaApi.filterPath(location.pathname);
+
+        // the ipad already smoothly scrolls and does not detect the scrollable
+        // element if top=0; as such we disable this behaviour for the iPad
+        if (!jQuery.browser.ipad) {
+            jQuery('a[href*=#]').each(function () {
+                var thisPath = JoomlaApi.filterPath(this.pathname) || locationPath;
+                if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+                    var target = decodeURIComponent(this.hash.replace(/#/, ''));
+                    // note: I'm using attribute selector, because id selector can't match elements with '$'
+                    var $target = jQuery('[id="' + target + '"]');
+
+                    if ($target.length > 0) {
+                        jQuery(this).click(function (event) {
+                            var scrollElem = JoomlaApi.scrollableElement('html', 'body'),
+                                targetOffset = $target.offset().top;
+
+                            event.preventDefault();
+                            jQuery(scrollElem).animate({scrollTop: targetOffset}, 400, function () {
+                                location.hash = target;
+                            });
+                        });
+                    }
+                }
+            });
+        }
+
+        // Hide API Documentation menu if it's empty
+        jQuery('.subnav .dropdown a[id=api]').next().filter(function (el) {
+            if (jQuery(el).children().length == 0) {
+                return true;
+            }
+        }).parent().hide();
+    });
+}(jQuery, window, document, navigator);
